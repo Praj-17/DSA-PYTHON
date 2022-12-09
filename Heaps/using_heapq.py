521b9fa60163c6f337cc16c17955071f1c45a228
Prajwal Waykos <90028385+Praj-17@users.noreply.github.com>
2022-12-09 21:34:40 +0530

    completed heaps

---
+++ Heaps/using_heapq.py
@@ -0,0 +1,36 @@
+# Python3 program to demonstrate working of heapq
+
+'''
+Using Library functions:
+We use heapq class to implement Heap in Python. By default Min Heap is implemented by this class. But we multiply each value by -1 so that we can use it as MaxHeap. 
+
+'''
+
+from heapq import heappop, heappush, heapify
+
+# Creating empty heap
+heap = []
+heapify(heap)
+
+# Adding items to the heap using heappush
+# function by multiplying them with -1
+heappush(heap, -1 * 10)
+heappush(heap, -1 * 30)
+heappush(heap, -1 * 20)
+heappush(heap, -1 * 400)
+
+# printing the value of maximum element
+print("Head value of heap : " + str(-1 * heap[0]))
+
+# printing the elements of the heap
+print("The heap elements : ")
+for i in heap:
+	print((-1*i), end=" ")
+print("\n")
+
+element = heappop(heap)
+
+# printing the elements of the heap
+print("The heap elements : ")
+for i in heap:
+	print(-1 * i, end = ' ')
