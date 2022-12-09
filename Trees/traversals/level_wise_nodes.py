159bd469b8b32052fcc218355b6d6edc80b11382
Prajwal Waykos <90028385+Praj-17@users.noreply.github.com>
2022-12-09 11:07:14 +0530

    added levelwise nodes

---
+++ Trees/traversals/level_wise_nodes.py
@@ -0,0 +1,21 @@
+def get_level_wise_nodes(root):  
+    if not root:
+        return []
+    
+    queue = [root]
+    next_queue = []
+    level = []
+    result = []
+    
+    while queue:
+        for root in queue:
+            level.append(root.val)
+            if root.left:
+                next_queue.append(root.left)
+            if root.right:
+                next_queue.append(root.right)
+        result.append(level)
+        level = []
+        queue = next_queue
+        next_queue = []
+    return result
